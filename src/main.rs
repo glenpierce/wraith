fcdc9cad5b12464f0ea65ba0c67a5336a7844a54
Glen Pierce <glen.piercev@gmail.com>
2019-07-13 21:14:18 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+//Steganography is the practice of concealing a file, message, image, or video within another file, message, image, or video. The word steganography combines the Greek words steganos (στεγανός), meaning "covered, concealed, or protected", and graphein (γράφειν) meaning "writing".
+
+use std::env;
+use std::fs::File;
+use std::io::{BufReader, Read};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let fileName = &args[0];
+
+//    let mut data:Vec<u32> = Vec::new();
+    let mut data = String::new();
+    let file = File::open(fileName.as_str()).expect("Unable to open file");
+
+    let mut bufReader = BufReader::new(file);
+    bufReader.read_to_string(&mut data).expect("Unable to read string");
+    println!("{}", data);
+}
