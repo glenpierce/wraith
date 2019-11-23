574ae8d53aa667e168c10d13eae989bdee705bfa
Glen Pierce <glen.piercev@gmail.com>
2019-11-22 18:58:34 -0800

    Writes file back as "encrypted". It's not encrypted (yet).

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,36 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, Read, BufWriter, Write};
+//use std::io::{BufReader, Read, BufWriter, Write, Cursor};
+
+
+//extern crate crypto;
+//extern crate rand;
+//extern crate aesstream;
+//
+//use crypto::aessafe::{AesSafe128Encryptor, AesSafe128Decryptor};
+//use rand::{Rng, OsRng};
+//use aesstream::{AesWriter, AesReader};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let fileName: &String = &args[1];
-    println!("{}", fileName);
+    let file_name: &String = &args[1];
+    println!("{}", file_name);
 
     let mut data:Vec<u8> = Vec::new();
-//    let mut data = String::new();
-    let file = File::open(fileName.as_str()).expect("Unable to open file");
 
-    let mut bufReader = BufReader::new(file);
-    bufReader.read_to_end(&mut data).expect("Unable to read string");
+    let file = File::open(file_name.as_str()).expect("Unable to open file");
+
+    let mut buf_reader = BufReader::new(file);
+    buf_reader.read_to_end(&mut data).expect("Unable to read string");
     println!("{:?}", data);
 
-//    let newFile = File::create("encrypted").expect("Unable to create file");
-//    let mut newFile = BufWriter::new(newFile);
-//    newFile.write_all(data.as_bytes()).expect("Unable to write data");
+    let new_file = File::create("encrypted").expect("Unable to create file");
+    let mut new_file = BufWriter::new(new_file);
+    let mut pos = 0;
+    while pos < data.len() {
+        let bytes_written = new_file.write(&data[pos..]).expect("unable to write to file");
+        pos += bytes_written;
+    }
 }
